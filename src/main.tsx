e8401b53b23cc74b21977cba64d7b181cd18a7e3
NigthDreamer <migthdreams@gmail.com>
2024-07-10 19:38:46 +0200

    Se crea el contexto del mapa

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { MapsApp } from './MapsApp.tsx'
+
+if( !navigator.geolocation ) {
+  alert('Tu navegador no tiene geolocalizacion');
+  throw new Error('Tu navegador no tiene geolocalizacion')
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MapsApp />
   </React.StrictMode>,
 )
